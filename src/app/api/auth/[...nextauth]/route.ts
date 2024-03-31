df0d6ca0485f46a40a30a96a6973cde199d87ccb
deep-git <deepakthangella5@gmail.com>
2024-03-31 17:30:06 -0400

    Fix database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,4 @@
-import bcrypt from "bcrypt";
 import NextAuth from "next-auth/next";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-import prisma from "@/lib/prismadb";
-import Credentials from "next-auth/providers/credentials";
 import { authOptions } from "@/utils/authOptions";
 
 const handler = NextAuth(authOptions);
