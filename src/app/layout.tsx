63461da5670a627f4f8173bb386b4c74bdddb7cc
deep-git <deepakthangella5@gmail.com>
2024-03-31 17:39:02 -0400

    Fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
 import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import MainNavbar from "@/components/navbar/main-navbar";
-import ToasterContext from "./context/ToastContext";
 import AuthContext from "./context/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
+import { open_sans } from "@/lib/font";
 
-const open_sans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blue Docs",
   description: "Your powerful document application",
-  icons: {
-    icon: "/favicon_wave.jpg"
-  }
 };
 
 export default function RootLayout({
@@ -31,7 +25,7 @@ export default function RootLayout({
               {children}
             </div>
           </main>
-          <Toaster/>
+          <Toaster />
         </AuthContext>
       </body>
     </html>
